3c10f8668ee2b627fa3589fcab8229e141552747
Oleg Bulavitchi <oleg@olegs-mbp.home>
2016-04-26 11:48:47 +0100

    yield curve to flat table

--- test/table-transform.js
+++ test/table-transform.js
@@ -1,3 +1,5 @@
+import {expect} from 'chai';
+
 function sum(m) {
     return Object.keys(m).reduce((s, k) => {
         const val = m[k];
@@ -7,8 +9,10 @@ function sum(m) {
         return s;
     }, 0)
 }
-function sub(tree, coll) {
-    const subFields = Object.keys(tree);
+function sub(filter, tree, coll) {
+    const subFields = Object.keys(tree).filter(sf => {
+        return (filter !== undefined) ? filter === sf : true
+    });
 
     subFields.reduce((c, subField) => {
         const existing = c[subField];
@@ -21,7 +25,39 @@ function sub(tree, coll) {
     }, coll);
 }
 
-function sumTable(tree, fieldName) {
+function getAsTuple(ob) {
+    return Object.keys(ob).reduce((coll, f)=> {
+        coll.push([f, ob[f]]);
+        return coll;
+    }, []);
+}
+
+function sub1(filter, tree, coll) {
+    const subFields = Object.keys(tree).filter(sf => {
+        return (filter !== undefined) ? filter === sf : true
+    });
+
+    subFields.forEach(subField => {
+        getAsTuple(tree[subField])
+            .forEach(tuple => {
+                const
+                    fieldName = tuple[0],
+                    fieldValue = tuple[1],
+                    existing = coll[fieldName];
+                if (existing) {
+                    existing.values.push(fieldValue);
+                } else {
+                    coll[fieldName] = {name: fieldName, type: 'double', values: [fieldValue]}
+                }
+
+            });
+
+
+    });
+
+}
+
+function sumTable(tree, fieldName, sub) {
     const mainFields = Object.keys(tree);
 
     let hashed = mainFields.reduce((columns, mainField)=> {
@@ -43,6 +79,9 @@ function sumTable(tree, fieldName) {
     }, []);
 }
 
+function partial(f, v) {
+    return f.bind(undefined, v);
+}
 
 
 describe('flatten hierarchy', () => {
@@ -50,7 +89,7 @@ describe('flatten hierarchy', () => {
             GBP: {
                 0.0: {
                     DV01: {s1: 10, s2: 11},
-                    NPV: {s1: 12, s3: 13}
+                    NPV: {s1: 12, s2: 13}
                 },
                 1.0: {
                     DV01: {s1: -1, s2: -2},
@@ -61,7 +100,7 @@ describe('flatten hierarchy', () => {
         total_expected = [{
             name: 'ladder',
             type: 'double',
-            values: [0.0, 1.0]
+            values: ['0', '1']
 
         },
             {
@@ -73,11 +112,35 @@ describe('flatten hierarchy', () => {
                 name: 'NPV',
                 type: 'double',
                 values: [25, -7]
-            }];
+            }],
+        npv_expected = [
+            {name: 'ladder', type: 'double', values: ['0', '1']},
+            {name: 's1', type: 'double', values: [12, -4]},
+            {name: 's2', type: 'double', values: [13, -3]}
+        ],
+        dv01_expected = [
+            {name: 'ladder', type: 'double', values: ['0', '1']},
+            {name: 's1', type: 'double', values: [10, -1]},
+            {name: 's2', type: 'double', values: [11, -2]}
+        ];
 
 
-    it('should be able to flat it', () => {
-        console.log(sumTable(tree['GBP'], 'ladder'));
+    it('should be able to flat it and calculate total', () => {
+        const totals = sumTable(tree['GBP'], 'ladder', partial(sub));
+        expect(totals).to.deep.equal(total_expected);
+        console.log(totals);
+    });
+
+    it('should flatten NPV and have security details', ()=> {
+        const totals = sumTable(tree['GBP'], 'ladder', partial(sub1, 'NPV'));
+        expect(totals).to.deep.equal(npv_expected);
+        console.log(totals);
+    });
+
+    it('should flatten NPV and have security details', ()=> {
+        const totals = sumTable(tree['GBP'], 'ladder', partial(sub1, 'DV01'));
+        expect(totals).to.deep.equal(dv01_expected);
+        console.log(totals);
     });
 
 });
